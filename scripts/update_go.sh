2b44553788462f9b3f3bcae28ca7a8f6beb36427
Shubham Hibare <shubham@hibare.in>
2023-05-07 23:23:16 +0530

    fix version

--- scripts/update_go.sh
+++ scripts/update_go.sh
@@ -13,7 +13,7 @@ wget -q https://golang.org/dl/$GO_LATEST_VERSION.linux-amd64.tar.gz
 
 # Delete previous & unpack the archive
 echo "Installing Go $GO_LATEST_VERSION"
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.3.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf $GO_LATEST_VERSION.linux-amd64.tar.gz
 
 # Add Go to the system path if it's not already there
 if ! grep -q '/usr/local/go/bin' "$HOME/.bashrc"; then
